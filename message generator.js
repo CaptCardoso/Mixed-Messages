dfa95b792b4315927a9b901d2c2b480656cf70c5
Afolabi Cardoso <cardosoafolabi@yahoo.com>
2021-10-09 15:45:00 -0400

    Add files via upload

--- message generator.js
+++ message generator.js
@@ -1,16 +1,21 @@
 //function to generate random number
 
-function generateRandomNumber (num){
-    return Math.floor(Math.random() * num)
+function generateRandomNumber (){
+    return Math.floor(Math.random() * 3)
 }
-console.log(generateRandomNumber(9));
 
-//The topic is boxers and words they are famous for
+//create a list of favorite boxers
+const boxers = ["Ali", "Wilder", "Roy Jones Jnr"]
 
-//create objects with of key/value pairs, with the boxers as keys and famous words as values
+//create a list with the boxers and their famous words
+const boxersPhrase = ["I fly like a butterfly, sting like a bee", "Bomb Squad", "Y'all must have forgot"]
 
-const boxers = {
-    Ali: "I fly like a butterfly, sting like a bee",
-    Wilder: "Bomb Squad",
-    Roy_Jones: "Y'all must have forgot"
+//create a list with boxers and thier DOB
+const boxersDOB = [ "January 17, 1942", "October 22, 1985", "January 16, 1969"]
+
+//create function to generate message
+function messageGenerator (){
+    num = generateRandomNumber();
+    console.log(`Boxer: ${boxers[num]} \nDOB: ${boxersDOB[num]} \nFavorite phrase: ${boxersPhrase[num]} `)
 }
+messageGenerator()
