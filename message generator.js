4419a1e1ca9c7b35d540e4b6da40d7a34d715dcb
Cardoso Afolabi <cardosoafolabi@yahoo.com>
2021-10-09 15:56:06 +0100

    commit

---
+++ message generator.js
@@ -0,0 +1,12 @@
+//function to generate random number
+
+function generateRandomNumber (num){
+    return Math.floor(Math.random() * num)
+}
+console.log(generateRandomNumber(9));
+
+//Topic is favourite boxers bio
+
+//create objects with of key/value pairs, with the boxers as keys and their bio as the values
+
+
